990a37602705e84bf6bff937e6e690a6c4c83dd7
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-06-21 12:23:51 +0200

    refactor to functions

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class BookList extends Component {
-	render() {
-		return (
-			<div>
-				<div className="book-list">
-					<ul>
-						<li>El imperio final</li>
-						<li>El pozo de la ascensión</li>
-						<li>El Héroe de las eras</li>
-					</ul>
-				</div>
-			</div>
-		);
-	}
-}
+const BookList = () => {
+	return (
+		<div className="book-list">
+			<ul>
+				<li>El imperio final</li>
+				<li>El pozo de la ascensión</li>
+				<li>El Héroe de las eras</li>
+			</ul>
+		</div>
+	);
+};
+
+export default BookList;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './Header.module.css';
 
-export default class Header extends Component {
-	render() {
-		return (
+const Header = () => {
+	return (
+		<div>
 			<div className={styles.header}>
 				<h1>Reading List</h1>
 				<p>Currently you have 3 books to read</p>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
+
+export default Header;

480994cb5039a640acb5282421d080a05b72becc
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-06-21 11:21:45 +0200

    Create Navbar.js

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Context App</h1>
+				<ul>
+					<li>Home</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
+			</div>
+		);
+	}
+}

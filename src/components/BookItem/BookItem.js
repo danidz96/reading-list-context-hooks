5f4c673a384d55f3f941b976f3866fcd38956632
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-06-21 13:33:10 +0200

    style book items

--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import styles from './BookItem.module.css';
 
 const BookItem = (props) => {
 	const { book } = props;
 	return (
-		<div>
-			<h3>{book.title}</h3>
-			<p>{book.author}</p>
+		<div className={styles.book}>
+			<div className="book-data">
+				<h3 className={styles.title}>{book.title}</h3>
+				<p className={styles.author}>{book.author}</p>
+			</div>
+			<i className={`fa fa-trash-o ${styles.icon}`} />
 		</div>
 	);
 };

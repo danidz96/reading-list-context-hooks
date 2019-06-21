9367228ee9bec8c4db5f8f9912838d7c4fbef4ca
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-06-21 11:25:37 +0200

    add navbar and booklist

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import BookList from './components/BookList/BookList';
 
 const App = () => {
-	return <div className="App">App</div>;
+	return (
+		<div className="App">
+			<Navbar />
+			<BookList />
+		</div>
+	);
 };
 
 export default App;

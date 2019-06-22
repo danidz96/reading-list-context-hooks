7a3fd84b19a27d6a769748a6e3279785896ad848
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-06-22 15:41:17 +0200

    add button for add book

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import BookItem from '../BookItem/BookItem';
 import styles from './BookList.module.css';
+import Button from '../Button/Button';
 
 const BookList = () => {
 	const [ books, setBooks ] = useState([
@@ -9,7 +10,14 @@ const BookList = () => {
 		{ title: 'El Héroe de las Eras', author: 'Brandon Sanderson' }
 	]);
 
-	return <ul className={styles.list}>{books.map((book) => <BookItem book={book} key={book.title} />)}</ul>;
+	return (
+		<React.Fragment>
+			<div className={styles.addBookBtn}>
+				<Button />
+			</div>
+			<ul className={styles.list}>{books.map((book) => <BookItem book={book} key={book.title} />)}</ul>;
+		</React.Fragment>
+	);
 };
 
 export default BookList;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+const Button = (props) => <input type="button" value="Add Book" className={styles.button} />;
+
+export default Button;

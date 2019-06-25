7d89082b085ca5876247be5613b4f41fe0d2f97f
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-06-25 21:48:42 +0200

    Updates

---
+++ precache-manifest.8c3567101158af29846fe74b0fde2ddc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb25ced15c07ad429a1af3d5e4b7c37",
+    "url": "/reading-list-context-hooks/index.html"
+  },
+  {
+    "revision": "93a07d7c9b947f8bb987",
+    "url": "/reading-list-context-hooks/static/css/main.016a9764.chunk.css"
+  },
+  {
+    "revision": "1c65841baee2cc061948",
+    "url": "/reading-list-context-hooks/static/js/2.1595fad6.chunk.js"
+  },
+  {
+    "revision": "93a07d7c9b947f8bb987",
+    "url": "/reading-list-context-hooks/static/js/main.36139430.chunk.js"
+  },
+  {
+    "revision": "9266ab7f6212cee68d3f",
+    "url": "/reading-list-context-hooks/static/js/runtime~main.205e9b6e.js"
+  }
+]);

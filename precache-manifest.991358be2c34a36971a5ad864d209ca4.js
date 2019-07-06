d46d358a83b1f4ba44e0e5d97496de2dcdccb69e
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-07-06 15:30:43 +0200

    Updates

--- precache-manifest.991358be2c34a36971a5ad864d209ca4.js
+++ precache-manifest.991358be2c34a36971a5ad864d209ca4.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "23914cb1d90fb41ca03dfabd73f26df3",
+    "revision": "43ecd93a467314a485466a9ee2a83227",
     "url": "/reading-list-context-hooks/index.html"
   },
   {
-    "revision": "39945e9c67c8149a3bdc",
+    "revision": "8e636fc6cbbc945a96c1",
     "url": "/reading-list-context-hooks/static/css/main.016a9764.chunk.css"
   },
   {
-    "revision": "1c65841baee2cc061948",
-    "url": "/reading-list-context-hooks/static/js/2.1595fad6.chunk.js"
+    "revision": "45c45c9cb8149c6ef69e",
+    "url": "/reading-list-context-hooks/static/js/2.2688b989.chunk.js"
   },
   {
-    "revision": "39945e9c67c8149a3bdc",
-    "url": "/reading-list-context-hooks/static/js/main.e14da5ed.chunk.js"
+    "revision": "8e636fc6cbbc945a96c1",
+    "url": "/reading-list-context-hooks/static/js/main.987d4bc0.chunk.js"
   },
   {
     "revision": "9266ab7f6212cee68d3f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reading-list-context-hooks/precache-manifest.6b32391af6bfe4f18385fdeb875429cb.js"
+  "/reading-list-context-hooks/precache-manifest.991358be2c34a36971a5ad864d209ca4.js"
 );
 
 self.addEventListener('message', (event) => {

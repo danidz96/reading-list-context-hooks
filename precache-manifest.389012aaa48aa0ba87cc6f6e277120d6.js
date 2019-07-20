175381bb46ff295184ba2c2b988073287b35ec01
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-07-20 15:19:29 +0200

    Updates

--- precache-manifest.389012aaa48aa0ba87cc6f6e277120d6.js
+++ precache-manifest.389012aaa48aa0ba87cc6f6e277120d6.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1d955dc2bc47d798a4132e405816fbd1",
+    "revision": "c19fced0331c0a3c175e664e8d03d0c2",
     "url": "/reading-list-context-hooks/index.html"
   },
   {
-    "revision": "95fb1486134c753c34d3",
+    "revision": "396a37602ed94819b7f5",
     "url": "/reading-list-context-hooks/static/css/main.016a9764.chunk.css"
   },
   {
-    "revision": "510824418de21d7d0b60",
-    "url": "/reading-list-context-hooks/static/js/2.ac98acc6.chunk.js"
+    "revision": "12dadc446cd895798475",
+    "url": "/reading-list-context-hooks/static/js/2.c5d8ef88.chunk.js"
   },
   {
-    "revision": "95fb1486134c753c34d3",
-    "url": "/reading-list-context-hooks/static/js/main.8e2f23fd.chunk.js"
+    "revision": "396a37602ed94819b7f5",
+    "url": "/reading-list-context-hooks/static/js/main.1b66f256.chunk.js"
   },
   {
     "revision": "9266ab7f6212cee68d3f",
